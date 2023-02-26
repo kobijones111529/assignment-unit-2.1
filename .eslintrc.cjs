88623a569fdacfd7720bb24b0c8f7384fc196722
Kobi Jones <kobijonesmail@gmail.com>
2023-02-25 22:53:02 -0600

    Add ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: 'standard',
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: ['warn', 'always']
+  }
+};
